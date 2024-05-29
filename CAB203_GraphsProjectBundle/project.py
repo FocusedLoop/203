f9dae94f0ec78d931878774ceb6c680e2062351d
FocusedLoop <43334820+FocusedLoop@users.noreply.github.com>
2024-05-29 19:00:37 +1000

    finished

--- CAB203_GraphsProjectBundle/project.py
+++ CAB203_GraphsProjectBundle/project.py
@@ -2,44 +2,7 @@ import graphs
 import digraphs
 import csv
 
-#remove
-import os
-scriptDirectory = os.path.dirname(__file__)
-
-
 def gamesOK(games):
-    #Degree, Connected
-    #Distance
-    #bipartition, minColouring
-    #isIndependentSet
-    
-    #pathFromTree
-    
-    #Notes from chatgpt
-    #N will show all the neighbours of a player
-    #NS will show all the neighbours of 2 players
-    #
-    
-    #OLD CODE
-    player_database = {}
-    #tutorial_6_april11 line 1 to 13, change
-    #first_elements = {player_1 for (player_1, player_2) in games}
-    #second_elements = {player_2 for (player_1, player_2) in games}
-    #players = first_elements | second_elements
-    #U, T = graphs.bipartition(players, games)
-    #test = digraphs.maxMatching(players, players, games)
-    #test = graphs.bipartition(players, games)
-    #graphs.assertIsUndirectedGraph(players, games)
-    
-    #print(players)
-    #print(games)
-    #print(test)
-    
-    #graphs.connected(players, games)
-    
-    #graphs.isIndependentSet()
-    
-    #NEW CODE
     # Evey player needs to play against or they need 2 games
     # List all the players
     first_elements = {player_1 for (player_1, player_2) in games}
@@ -47,159 +10,88 @@ def gamesOK(games):
     players = first_elements | second_elements
     
     # Add edges to ensure graph is undirected
-    games = games | { (b, a) for (a,b) in games } # symmetrise
+    # Reference: Tutorial 7 python code
+    games = games | { (b, a) for (a,b) in games }
     
-    # Find all possible neighbours of the players
-    # V = players
-    # E = games
-    # u = player
-    opponents = {}
-    first = list(games)[0]
-    # Check if they have 2 players in common
-    for player in sorted(players):
-        #print(player)
-        #print(first)
-        opponents[player] = (graphs.N(players, games, player))
-        #print(opponents[player])
-        #print(graphs.N(players,games, list(opponents[player])[0]))
-        #print(graphs.N(players,games, list(opponents[player])[1]))
-        #if not graphs.connected(graphs.NS(players,games, opponents[player]), games):
-            #print(graphs.N(players, games, player))
-            #print(graphs.NS(players,games, opponents[player]))
-            #print(list(opponents[player])[0])
-            #return False
-        #if graphs.N(players,games, list(opponents[player])[0]) != graphs.N(players,games, list(opponents[player])[1]):
-            #return False
+    # Set V to all players and E to the games
+    V = players
+    E = games
     
-    # All games have the same number of games
-    opponent_size = graphs.degree(opponents, games, (list(opponents.keys()))[0])# arbitrary?
-    for opponent in opponents:
-        if graphs.degree(opponents, games, opponent) != opponent_size:
-            return False
-        opponent_size = graphs.degree(opponents, games, opponent)
+    # Check if all players play at most one game
+    opponent_sizes = [graphs.degree(V, E, opponent) for opponent in V]
+    if len(set(opponent_sizes)) != 1:
+        return False
+
+    # Go through each players opponent (neighbour) in the verticies
+    for player in V:
+        for player_opponent in V:
+            # Check if the player isnt the opponent
+            if player != player_opponent:
+                # Check if the player plays against the opponent
+                if (player, player_opponent) in E or (player_opponent, player) in E:
+                    continue
+                # Check if the player and opponent share atleast 2 neighbours
+                neighbors_player = graphs.N(V, E, player)
+                neighbors_player_opponent = graphs.N(V, E, player_opponent)
+                common_neighbors = neighbors_player & neighbors_player_opponent
+                if len(common_neighbors) < 2:
+                    return False
     return True
 
 def referees(games, refereecsvfilename):
-    
-    #NEW CODE
+    # Read the csv file and create a dictionary of referee conflicts, referee (key) and conflicts(value)
+    # Reference: lecture 6 Python CSV DictReader
     rows = {}
     with open(refereecsvfilename, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             rows[row['Referee']] = (row['Conflict1'], row['Conflict2'], row['Conflict3'], row['Conflict4'])
     
-    #rows = {key: tuple(filter(lambda x: x is not None, value)) for key, value in rows.items()}
-    # Get the refs and the games seperate
-    #print(rows)
+    # Set V as the verticies of all games and referees
     V = games.union(list(rows.keys()))
+    
+    # Set E as the edges
     E = set()
-    #E = {((player_1, player_2), key) }
-    # get all the players
+    
+    # Get all the players
     first_elements = {player_1 for (player_1, player_2) in games}
     second_elements = {player_2 for (player_1, player_2) in games}
     players = first_elements | second_elements
-    #print(games)
-    #print(players)
-    #print(rows)
-    #print(V)
     
+    # Create a list of all games and referees
     games_list = list(games)
     referees = list(rows.keys())
-    #print(referees)
+    
+    # For each game check each of the conflicts
     for game in range(len(games_list)):
         for conflicts in rows.values():
-            #print(conflicts)
             for conflict in range(len(conflicts)):
-                #print(conflict)
+                # Check if that conflict is not in the game
                 if list(conflicts)[conflict] not in games_list[game]:
+                    # For each referee create an undirected edge for each referee from game to referee
                     for referee in range(len(referees)):
                         E.add((games_list[game], referees[referee]))
                         E.add((referees[referee], games_list[game]))
-    #print(E)
+                        
+    # Put V and E into the bipartion function and return to seperate sets, games and referees
     A, B = graphs.bipartition(V, E)
-    #print(A)
-    #print(B)
-    matched_games = digraphs.maxMatching(A, B, E)
-    print(matched_games)
-    matched_games_dict = {}
-    for match in matched_games:
-        if isinstance(match[0], tuple):
-            # When the first element is a game pair
-            game_pair, referee = match
-        else:
-            # When the first element is a referee
-            referee, game_pair = match
-
-            matched_games_dict[game_pair] = referee
-
     
-    #print(matched_games_dict)
+    # Match the referees and game returned by the bipartition using the edges
+    matched_games = digraphs.maxMatching(A, B, E)
+    
+    # Create a dictionary from the results of matched_games
+    # Set the referee to the value and game to the key
+    matched_games_dict = {}
+    for matched_game in matched_games:
+        if isinstance(matched_game[0], tuple):
+            player_game, referee = matched_game
+        else:
+            referee, player_game = matched_game
+            matched_games_dict[player_game] = referee
 
     return matched_games_dict
     
-    
-    #print(E)
-    #E = E | { (b, a) for (a,b) in E } # makes the graph undirected
-    #A, B = graphs.bipartition(V, E)
-    #print(A)
-    #print(B)
-    #print(digraphs.maxMatching(A, B, E))
-    #V = games.union(list(rows.keys()))
-    # Get all the conflicts
-    # Players refs can ref
-    #new_dict = {key: tuple(v for v in set().union(*rows.values()) if v not in rows[key]) for key in rows}
-    # Remove refs from players can ref
-    #new_dict = {k: tuple(v for v in val if v != k) for k, val in new_dict.items()}
-    # Remove None players
-    #new_dict = {key: tuple(value for value in values if value is not None) for key, values in new_dict.items()}
-    #E = set([((x, y), key) for key, values in new_dict.items() for i, x in enumerate(values) for y in values[i+1:]])
-    #E = {((player_1, player_2), key) }
-    #print(valid_pairs)
-    #E = E | { (b, a) for (a,b) in E } # makes the graph undirected
-    
-    #print('\n')
-    #print(E)
-    #A, B = graphs.bipartition(V, E)
-    #print(A)
-    #print(B)
-    #print(digraphs.maxMatching(A, B, games))
-    #E = - conflict
-    
 def gameGroups(assignedReferees):
-    
-    
-    #games = list(V)
-    #print(games)
-    #for game in games:
-    #    for player in range(games:
-    #    for player in range(i + 1, len(pairs)):
-    #        # Check if a player exists in the other game
-    #        if assignedReferees[games[player]] == assignedReferees[games[player + 1]]:
-    #            # Make the edges undicted
-    #            E.add((games[player], games[player + 1]))
-    #            E.add((games[player + 1], games[player]))
-            # Check if the games are the same
-    #        if set(games[game]) & set(games[game + 1]):
-    #            E.add((games[player], games[player + 1]))
-    #            E.add((games[player + 1], games[player]))
-            #if assignedReferee.values() in games:
-                #E.add((games[game], games[game+1]))
-            
-    
-    #pairs = list(assignedReferees.keys())
-    #refs = list(assignedReferees.values())
-    #for i in range(len(pairs)):
-    #    for j in range(i + 1, len(pairs)):
-    #        if refs[i] in pairs[j] or refs[j] in pairs[i]:
-    #            E.add((pairs[i], pairs[j]))
-    #            E.add((pairs[j], pairs[i]))
-    #        if assignedReferees[pairs[i]] == assignedReferees[pairs[j]]:
-    #            E.add((pairs[i], pairs[j]))
-    #            E.add((pairs[j], pairs[i]))
-    #        if set(pairs[i]) & set(pairs[j]):
-    #            E.add((pairs[i], pairs[j]))
-    #            E.add((pairs[j], pairs[i]))
-    
     # Setting V (verticies) to set of games and Create an empty set for E (edges)
     V = set(assignedReferees.keys())
     E = set()
@@ -229,20 +121,12 @@ def gameGroups(assignedReferees):
     # Create a coloured graph with V and E and get the chromatic colour
     k, C = graphs.minColouring(V, E)
     
-    #print("Chromatic number:", k)
-    #print("Coloring of the graph:", C)
-    
     # Create timeslots using the chromatic colour
     time_slots = graphs.colourClassesFromColouring(C)
-    #print("Time slots:", time_slots)
-    #print(V)
-    #print(E)
-    #print(time_slots)
     
     return time_slots
 
 def gameSchedule(assignedReferees, gameGroups):
-    
     # Freeze each set of games in game groups
     gameGroups_frozen = [frozenset(group) for group in gameGroups]
     
@@ -259,13 +143,10 @@ def gameSchedule(assignedReferees, gameGroups):
     
     # Go through each referee in V
     for ref in assignedReferees.values():
-        #refs.append(ref)
         # Go through each game group in the set of game groups
         for games in gameGroups:
             # Check all the elements in the game group
             for game in games:
-                #print(games)
-                #print(ref)
                 # Check if referee is playing in the game group
                 if any(ref in element for element in games):
                     # Create an edge from the game group to referee
@@ -282,79 +163,9 @@ def gameSchedule(assignedReferees, gameGroups):
     if game_order != None:
         game_order = [elem for elem in game_order if isinstance(elem, frozenset)]
         game_order = [set(fs) for fs in game_order]
-    #print(E)
-    #print("\n")
+
     return game_order
 
 def scores(p, s, c, games):
+    # augmentedFlow and maxFlow would be used here
    pass
-
-#games = { ( " Alice " , " Bob " ) , ( " Charlie " , " Bob " ) }
-#games = { ( " A ", " B " ), (" B ", " C "), (" C ", " D "), (" D ", " A ") }
-#games = { ( " A ", " B " ), ( " C ", " D " ) }
-#games = { (0, 1), (1, 2), (2, 3), (3, 4), (4, 0) }
-#games = { ( " A ", " C " ), ( " A ", " D " ), ( " B ", " C " ), ( " B ", " D ") }
-
-#print(gamesOK(games))
-refereecsvfilename = os.path.join(scriptDirectory, 'referees1.csv')
-#games = { ('Bob', 'Alice'), ('Joe', 'Charlie'), ('Elaine', 'Rene') }
-games = { ('Bob', 'Alice'), ('Joe', 'Charlie'), ('Ellie', 'Rene') }
-print(referees(games, refereecsvfilename))
-#assignedReferees = { 
-#         ('Alice', 'Bob'): 'Rene', 
-#         ('Elaine', 'Charlie'): 'Dave',
-#         ('Rene', 'Elaine'): 'Alice',
-#         ('Dave', 'Bob'): 'Charlie'
-#      }
-#assignedReferees = { ('Alice', 'Bob'): 'Rene'}
-#assignedReferees = { 
-#         ('Alice', 'Bob'): 'Rene', 
-#         ('Elaine', 'Charlie'): 'Dave',
-#         ('Rene', 'Elaine'): 'Alice',
-#         ('Dave', 'Bob'): 'Charlie',
-#         ('Alice', 'Rene'): 'Dave',
-#         ('Dave', 'Elaine'): 'Rene'
-#      }
-#print(gameGroups(assignedReferees))
-# test 3
-#assignedReferees = {
-#    ('Edward', 'Julia'): 'Faye Valentine',
-#    ('Faye Valentine', 'Edward'): 'Evalyn',
-#    ('Jet Black', 'Ein'): 'Spike Spiegel',
-#    ('Julia', 'Spike Spiegel'): 'Waymond',
-#    ('Spike Spiegel', 'Vicious'): 'Faye Valentine',
-#    ('Vicious', 'Jet Black'): 'Julia'
-#}
-
-#gameGroups = [
-#    {('Spike Spiegel', 'Vicious')},
-#    {('Vicious', 'Jet Black')},
-#    {('Jet Black', 'Ein'), ('Edward', 'Julia')},
-#    {('Faye Valentine', 'Edward'), ('Julia', 'Spike Spiegel')},
-#]
-#assignedReferees = {
-    #('Alice', 'Bob'): 'Charlie',
-    #('Charlie', 'Bob'): 'Rene'
-    #}
-#gameGroups = [
-    #{ ('Alice', 'Bob') },
-    #{ ('Charlie', 'Bob') },
-    #]
-#assignedReferees =  {
-#    ('Spike Spiegel', 'Vicious'): 'Jet Black',
-#    ('Edward', 'Julia'): 'Faye Valentine',
-#    ('Ein', 'Faye Valentine'): 'Julia',
-#    ('Faye Valentine', 'Edward'): 'Vicious',
-#    ('Jet Black', 'Ein'): 'Spike Spiegel',
-#    ('Julia', 'Spike Spiegel'): 'Ein',
-#    ('Vicious', 'Jet Black'): 'Edward'
-#    }
-
-#gameGroups = [
-#    {('Spike Spiegel', 'Vicious'), ('Ein', 'Faye Valentine')},
-#    {('Jet Black', 'Ein'), ('Edward', 'Julia')},
-#    {('Faye Valentine', 'Edward'), ('Julia', 'Spike Spiegel')},
-#    {('Vicious', 'Jet Black')}
-#    ]
-
-#print(gameSchedule(assignedReferees, gameGroups))
